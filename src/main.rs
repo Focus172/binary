5f122a17d70fe059375af52ffc74c5895f040d15
Focus172 <evan.stokdyk@gmail.com>
2023-03-12 13:22:07 -0700

    Patch: dumb people

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ fn main() {
         println!("1. Convert from Binary to Decimal");
         println!("2. Convert from Decimal to Binary");
         print!("Enter your choice: "); std::io::stdout().flush().unwrap();
-        
+
         let mut buffer = String::new();
         let _ = std::io::stdin().read_line(&mut buffer);
-        
+
         //checks if the input is a number
         let num = buffer.trim().parse::<i64>();
 
@@ -28,7 +28,10 @@ fn main() {
     match num {
         1 => to_binary(),
         2 => to_decimal(),
-        _ => println!("Invalid choice"),
+        _ => {
+            println!("Invalid choice");
+            main();
+        },
     }
 }
 
@@ -38,10 +41,11 @@ fn to_decimal() {
 
     let mut dec_buffer = String::new();
     let _ = std::io::stdin().read_line(&mut dec_buffer);
-    let mut dec = match dec_buffer.trim().parse::<i64>() {
+    let mut dec = match dec_buffer.trim().parse::<i128>() {
         Ok(number) => number,
         Err(_) => {
             println!("Do better");
+            to_decimal();
             return;
         }
     };
@@ -62,32 +66,24 @@ fn to_binary() {
     println!("You chose to convert from Binary to Decimal");
     print!("Enter a binary number: "); std::io::stdout().flush().unwrap();
 
-
-
     let mut bin_buffer = String::new();
     let _ = std::io::stdin().read_line(&mut bin_buffer);
-    let mut bin = match bin_buffer.trim().parse::<i64>() {
-        Ok(number) => number,
-        Err(_) => {
-            println!("Do better");
-            return;
-        }
-    };
+    let bin: &str = bin_buffer.trim();
 
-    let mut dec = 0;
+    let mut dec: i128 = 0;
     let mut count = 0;
-    while bin > 0 {
-        let dig = bin % 10;
-        match dig {
-            0 => (),
-            1 => dec += 2_i64.pow(count),
+    
+    for c in bin.chars().rev() {
+        match c {
+            '0' => (),
+            '1' => dec += 2_i128.pow(count),
             _ => {
-                println!("Invalid binary number");
+                println!("Invalid binary number, try again");
+                to_binary();
                 return;
             }
         }
         count += 1;
-        bin /= 10;
     }
     println!("Out: {}", dec);
 }
